bde5f6571d99565ca708ba80cd30e3274e9782ed
Javier Alejandro Claros Moreno <jclaros@agetic.gob.bo>
2018-10-09 09:15:24 -0700

    Primer Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Javier Claros on 10/9/18.
+//  Copyright © 2018 8ball. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        randomDiceIndex1 = Int.random(in: 0 ... 5)
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
+    }
+    
+}
+
