ce60ef1e9c3c5565f4c21885eea2c0f263d7188b
Javier Alejandro Claros Moreno <jclaros@agetic.gob.bo>
2018-10-09 16:01:50 -0400

    Agregue Funciones y Detección de sacudida

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice5"]
+    
     var randomDiceIndex1: Int = 0
     var randomDiceIndex2: Int = 0
 
@@ -17,16 +19,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView2: UIImageView!
     
     
-    override func viewDidLoad() {
+    override func viewDidLoad() {// Se ejecuta cuando la pantalla se carga
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages () {
         randomDiceIndex1 = Int.random(in: 0 ... 5)
         randomDiceIndex2 = Int.random(in: 0 ... 5)
         
-        print(randomDiceIndex1)
+        print(randomDiceIndex1, randomDiceIndex2)
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1] )
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2] )
     }
     
 }
